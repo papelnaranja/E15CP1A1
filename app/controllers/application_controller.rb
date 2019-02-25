e8402f404256b944463bb1eb6b547bb24c5fe7d8
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-02-25 20:52:15 -0300

    actividad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+  def authenticate_user!
+    redirect_to users_sign_in_path unless helpers.logged?
+  end
 end
